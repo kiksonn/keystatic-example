c74f2078958153bcbaa715dcc954463fcbaad095
kiksonn <kikson.yy@gmail.com>
2024-05-22 23:10:41 +0200

    created basic cms from docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+// astro.config.mjs
+import { defineConfig } from 'astro/config'
+
+import react from '@astrojs/react'
+import markdoc from '@astrojs/markdoc'
+  import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   integrations: [react(), markdoc(), keystatic()],
+   output: 'hybrid',
+})
