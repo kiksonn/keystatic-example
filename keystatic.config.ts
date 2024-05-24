371bc5014eb11e1ca67ad3860717f8ad194a6726
kiksonn <kikson.yy@gmail.com>
2024-05-24 17:14:16 +0200

    author

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,24 +1,42 @@
 // keystatic.config.ts
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: "local",
   },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*", // PATH TO MDOC FILES
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
+        title: fields.slug({ name: { label: "Title" } }),
         content: fields.document({
-          label: 'Content',
+          label: "Content",
           formatting: true,
           dividers: true,
           links: true,
           images: true,
+        }),
+        author: fields.relationship({
+          label: 'Authors',
+          collection: 'authors'
+        })
+      },
+    }),
+    authors: collection({
+      label: "Authors",
+      slugField: "name",
+      path: "src/content/authors/*",
+      format: { data: "json" },
+      schema: {
+        name: fields.slug({ name: { label: "Name" } }),
+        avatar: fields.image({
+          label: "Avatar",
+          directory: "public/images/avatars",
+          publicPath: '/images/avatars'
         }),
       },
     }),
