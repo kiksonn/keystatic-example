1ee5fd9ed3bf5206414297ea3f4cbe88228e8e0b
kiksonn <kikson.yy@gmail.com>
2024-06-13 02:48:54 +0200

    keystatic multple authors error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config'
-
-import react from '@astrojs/react'
-import markdoc from '@astrojs/markdoc'
-  import keystatic from '@keystatic/astro'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [react(), markdoc(), keystatic()],
-   output: 'hybrid',
-})
+  integrations: [react(), markdoc(), keystatic()],
+  output: 'hybrid',
+});
--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,4 +1,3 @@
-// keystatic.config.ts
 import { config, fields, collection } from "@keystatic/core";
 
 export default config({
@@ -9,35 +8,99 @@ export default config({
     posts: collection({
       label: "Posts",
       slugField: "title",
-      path: "src/content/posts/*", // PATH TO MDOC FILES
+      // where key static stores your data
+      path: "src/content/posts/*",
       format: { contentField: "content" },
       schema: {
         title: fields.slug({ name: { label: "Title" } }),
-        content: fields.document({
+        content: fields.markdoc({
           label: "Content",
-          formatting: true,
-          dividers: true,
-          links: true,
-          images: true,
+          options: {
+            image: {
+              directory: "src/assets/images/posts",
+              publicPath: "../../assets/images/posts/",
+            },
+          },
         }),
-        author: fields.relationship({
-          label: 'Authors',
-          collection: 'authors'
-        })
+
+        // create connection posts and authors
+
+        // author: fields.relationship({
+        //   label: 'Author',
+        //   collection: 'authors'
+        // }),
+
+        // create multiple authors on one project
+        authors: fields.array(
+          fields.relationship({
+            label: "Authors",
+            collection: "authors",
+            validation: {
+              isRequired: true,
+            },
+          }),
+          {
+            label: "Authors",
+            itemLabel: (item) => item.value || "Please select an author",
+          }
+        ),
       },
     }),
+    // Create new Collection Authors
     authors: collection({
       label: "Authors",
       slugField: "name",
       path: "src/content/authors/*",
+
+      // define format of authors
       format: { data: "json" },
+      // what field should be in avalible in collection
       schema: {
+        // auto generate slug
         name: fields.slug({ name: { label: "Name" } }),
+        // create avatar field
         avatar: fields.image({
           label: "Avatar",
+          // select where img avatar is store
           directory: "public/images/avatars",
-          publicPath: '/images/avatars'
+          publicPath: "/images/avatars",
         }),
+
+        // add showcase to authors
+        showcase: fields.blocks(
+          {
+            link: {
+              label: "Link",
+              schema: fields.object({
+                label: fields.text({
+                  label: "Label",
+                  validation: {
+                    length: {
+                      min: 1,
+                    },
+                  },
+                }),
+                url: fields.url({ label: "URL" }),
+              }),
+              itemLabel: (item) => "link : " + item.fields.label.value,
+            },
+            youtubeVideoId: {
+              label: "Youtube Video ID",
+              schema: fields.text({
+                label: "Youtube Video ID",
+                validation: {
+                  length: {
+                    min: 1,
+                  },
+                },
+              }),
+              itemLabel: (item) => " Youtube ID: " + item.value,
+            },
+          },
+          {
+            label: "Showcase",
+          }
+        ),
       },
     }),
   },
